4378cac4f8c83e7d854a6ba004141e34add77435
Chaka Hongoke <hongoke999@yahoo.com>
2020-06-09 14:48:23 +0300

    Destructuring

--- app.js
+++ app.js
@@ -1,31 +1,59 @@
-//  Create a symbol
-const sym1 = Symbol();
-const sym2 = Symbol('sym2');
+//  Destructuring Assignment
 
-// console.log(typeof sym2);
+let a, b;
 
-// console.log(Symbol('123') === Symbol('123'));
-// console.log(`Hello ${sym1.toString()}`);
+[a, b] = [100, 200];
 
-//  Unique Object Keys
-const KEY1 = Symbol();
-const KEY2 = Symbol('sym2');
+//  Rest pattern
+[a, b, c, ...rest] = [100, 200, 300, 400, 500];
 
-const myObj = {};
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(rest);
 
-myObj[KEY1] = 'Prop1';
-myObj[KEY2] = 'Prop2';
-myObj.key3 = 'Prop3';
-myObj.key4 = 'Prop4';
+({a, b} = {a: 100, b: 200, c: 300, d: 400, e: 500});
+({ a, b, ...rest } = { a: 100, b: 200, c: 300, d: 400, e: 500 });
 
-// console.log(myObj[KEY1]);
-// console.log(myObj[KEY2]);
+//  Array Destructuring
 
-//  Symbols are not enumerable in for...in
-/* for(let i in myObj) {
-  console.log(`${i}: ${myObj[i]}`);
-} */
+/* const people = ['John', 'Berth', 'Mike'];
 
-//  Symbols are ignored by JSON.stringify
-console.log(JSON.stringify({key: 'prop'}));
-console.log(JSON.stringify({[Symbol('sym1')]: 'prop' }));
+const [person1, person2, person3] = people;
+
+console.log(person1, person2, person3); */
+
+//  Parse array returned from function
+
+/* function getPeople() {
+  return ['John', 'Berth', 'Mike'];
+}
+
+let person1, person2, person3;
+[person1, person2, person3] = getPeople();
+
+console.log(person1, person2, person3); */
+
+//  Object Destructuring
+
+const person = {
+  name: 'John Doe',
+  age:  32,
+  city: 'Miami',
+  gender: 'Male',
+  sayHello: function() {
+    console.log('Hello');
+  }
+}
+
+//  Old ES5
+/* const name = person.name,
+      age = person.age,
+      city = person.city */;
+
+//  New ES6 Destructuring
+const { name, age, city, sayHello } = person;
+
+console.log(name, age, city);
+
+sayHello();
